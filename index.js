d2707b10d2ba1b3863d6bc17b81d55ae28120e22
Fredrik <fred.gerner@icloud.com>
2020-04-27 21:33:01 +1000

    added audio for drums

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 for (let i = 0; i < document.querySelectorAll('.drum').length; i++) {
     document.querySelectorAll('.drum')[i].addEventListener("click", function () {
-        audio = new Audio('sounds/tom-1.mp3')
+        switch (this.innerHTML) {
+            case 'w':
+                audio = new Audio('sounds/tom-1.mp3');
+                break
+            case 'a':
+                audio = new Audio('sounds/tom-2.mp3');
+                break
+            case 's':
+                audio = new Audio('sounds/tom-3.mp3');
+                break
+            case 'd':
+                audio = new Audio('sounds/tom-4.mp3');
+                break
+            case 'j':
+                audio = new Audio('sounds/snare.mp3');
+                break
+            case 'k':
+                audio = new Audio('sounds/crash.mp3');
+                break
+            default:
+                audio = new Audio('sounds/kick-bass.mp3');
+                break
+        }
         audio.play();
     });
 }
