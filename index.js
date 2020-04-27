31ab5ed3785519538eb94187c1653f7b203335f0
Fredrik Gerner <fredrik.gerner@me.com>
2020-04-27 12:25:01 +1000

    added sound to buttons and first background image

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 for (let i = 0; i < document.querySelectorAll('.drum').length; i++) {
     document.querySelectorAll('.drum')[i].addEventListener("click", function () {
-        alert("clicked!")
+        audio = new Audio('sounds/tom-1.mp3')
+        audio.play();
     });
 }
