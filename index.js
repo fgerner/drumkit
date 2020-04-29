664004c9cc83b4628ee91cc1a565712c57445476
Fredrik <fred.gerner@icloud.com>
2020-04-29 21:20:15 +1000

    added animation for drums and fixed audio bug.

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ for (let i = 0; i < document.querySelectorAll('.drum').length; i++) {
     document.querySelectorAll('.drum')[i].addEventListener("click", function () {
         let buttonInnerHtml = this.innerHTML;
         makeSound(buttonInnerHtml);
+        buttonAnimation(buttonInnerHtml);
 
     });
 
     //Key pressed
     document.addEventListener('keydown', function (event) {
         makeSound(event.key);
+        buttonAnimation(event.key);
     });
 
     // make sound
@@ -16,28 +18,43 @@ for (let i = 0; i < document.querySelectorAll('.drum').length; i++) {
         switch (key) {
             case 'w':
                 audio = new Audio('sounds/tom-1.mp3');
+                audio.play();
                 break
             case 'a':
                 audio = new Audio('sounds/tom-2.mp3');
+                audio.play();
                 break
             case 's':
                 audio = new Audio('sounds/tom-3.mp3');
+                audio.play();
                 break
             case 'd':
                 audio = new Audio('sounds/tom-4.mp3');
+                audio.play();
                 break
             case 'j':
                 audio = new Audio('sounds/snare.mp3');
+                audio.play();
                 break
             case 'k':
                 audio = new Audio('sounds/crash.mp3');
+                audio.play();
+                break
+            case 'l':
+                audio = new Audio('sounds/kick-bass.mp3');
+                audio.play();
                 break
             default:
-                audio = new Audio('sounds/kick-bass.mp3');
-                break
+                console.log('Shit got pressed!')
         }
-        audio.play();
-
-
     }
+}
+
+function buttonAnimation(currentKey) {
+    let activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add('pressed');
+
+    setTimeout(function () {
+        activeButton.classList.remove('pressed')
+    },100);
 }
